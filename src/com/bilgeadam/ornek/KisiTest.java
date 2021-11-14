2d80a2c0c41a7a9f914172fb125b9467c3cd93f9
mert.baskaya <mert.baskaya07@gmail.com>
2021-11-14 12:54:11 +0300

    Kişi Projesinin Son Hali

--- src/com/bilgeadam/ornek/KisiTest.java
+++ src/com/bilgeadam/ornek/KisiTest.java
@@ -2,7 +2,7 @@ package com.bilgeadam.ornek;
 
 public class KisiTest {
     public static void main(String[] args) {
-        Kisi kisi = new Kisi(1, "Mert", "Başkaya", 5000);
+        Kisi kisi = new Kisi(1, "Mert", "Başkaya", 35000);
 
         System.out.println(kisi);
     }
