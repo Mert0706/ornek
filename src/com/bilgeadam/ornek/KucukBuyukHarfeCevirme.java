14c8675490d6aa0166b2b6a25f6c76c6a1a3942f
mert <mert.baskaya07@gmail.com>
2021-11-21 13:07:43 +0300

    Küçük Büyük Harf Çevirme Eklendi

---
+++ src/com/bilgeadam/ornek/KucukBuyukHarfeCevirme.java
@@ -0,0 +1,32 @@
+package com.bilgeadam.ornek;
+
+import java.util.Locale;
+
+public class KucukBuyukHarfeCevirme {
+    public static void main(String[] args) {
+
+        String isim = "Mert";
+
+        //Büyük ve Küçük Harfe Çevirme.
+        System.out.println(isim.toLowerCase());
+        System.out.println(isim.toUpperCase());
+
+        String isim2 = "izmirli İĞDELER";
+        System.out.println(isim2.toUpperCase());
+
+        String isim3 = "IĞDIRLI ıspanaklar";
+        System.out.println(isim3.toLowerCase());
+
+        // Türkçe Karakter çevirme
+        System.out.println(isim3.replace("i", "İ").toUpperCase());
+        System.out.println(isim2.replace("I", "ı").toLowerCase());
+
+        //Local
+        Locale localeTR = new Locale("tr", "Tr");
+
+        String kelime = "izinli İSTANBUL";
+        System.out.println(kelime.toUpperCase(localeTR));
+
+
+    }
+}
